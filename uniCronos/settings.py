9340b3d0ce29d9350d0e5ad72ee002c821d001c6
AnaSofiR <asrodriguo@eafit.edu.co>
2023-11-16 02:18:02 -0500

    db

--- uniCronos/settings.py
+++ uniCronos/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'uniCronos.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'unicronos',
-        'USER': 'postgres',
-        'PASSWORD': 'Melia31111+',
-        'HOST': 'localhost',
-        'PORT': '5432',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
